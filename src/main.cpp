14dc20326ade5043442646d764340e5ef9eb749e
Harold <69054133+Haroldajnp21@users.noreply.github.com>
2022-01-22 16:52:16 -0500

    [22/01/2022 16:35] se introdujo las variables para el display 7 seg

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,30 @@
 #include <Arduino.h>
+// DISPLAY 7 SEGMENTOS CON ARDUINO 
+/*CONSTANTES PARA LOS PUERTOS*/
+#define PULSASOR 10
+
+byte numero [10][8]=
+{
+  { 1, 1, 1, 1, 1, 1, 0, 0 }, // 0
+  { 0, 1, 1, 0, 0, 0, 0, 0 }, // 1
+  { 1, 1, 0, 1, 1, 0, 1, 0 }, // 2
+  { 1, 1, 1, 1, 0, 0, 1, 0 }, // 3
+  { 0, 1, 1, 0, 0, 1, 1, 0 }, // 4
+  { 1, 0, 1, 1, 0, 1, 1, 0 }, // 5
+  { 1, 0, 1, 1, 1, 1, 1, 0 }, // 6
+  { 1, 1, 1, 0, 0, 0, 0, 0 }, // 7
+  { 1, 1, 1, 1, 1, 1, 1, 0 }, // 8
+  { 1, 1, 1, 0, 0, 1, 1, 0 }  // 9
+}
 
 void setup()
- {
+{
   // put your setup code here, to run once:
   
 }
 
 void loop()
- {
+{
   // put your main code here, to run repeatedly:
 
 }
