dc32475fd0642dc33ccb7a3825f5c5600e524d21
Harold <69054133+Haroldajnp21@users.noreply.github.com>
2022-01-22 17:02:47 -0500

    [22/01/2022 17:05] inclui en for

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 // DISPLAY 7 SEGMENTOS CON ARDUINO 
 /*CONSTANTES PARA LOS PUERTOS*/
 #define PULSASOR 10
-
+int i;
 byte numero [10][8]=
 {
   { 1, 1, 1, 1, 1, 1, 0, 0 }, // 0
@@ -15,13 +15,25 @@ byte numero [10][8]=
   { 1, 1, 1, 0, 0, 0, 0, 0 }, // 7
   { 1, 1, 1, 1, 1, 1, 1, 0 }, // 8
   { 1, 1, 1, 0, 0, 1, 1, 0 }  // 9
-}
+};
 
 void setup()
 {
   // put your setup code here, to run once:
+  // Iniciamos el monitor serie
+  Serial.begin(9600);
+  // Ponemos los pines de los segmentos en modo OUTPUT (salida)
+  for ( i = 2; i < 10; i++)
+  {
+    /* code */
+    pinMode(i,OUTPUT);
+  }
   
 }
+
+
+
+
 
 void loop()
 {
