b7de1b4b862bf58462d1f07a1b8c78b1181dfce4
Harold <69054133+Haroldajnp21@users.noreply.github.com>
2022-01-22 17:46:18 -0500

    [22/01/2022 17:46] YA COMPILADO

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 /*CONSTANTES PARA LOS PUERTOS*/
 #define PULSASOR 10
 int i;
+int e;
+#define TIEMPO 500
 byte numero [10][8]=
 {
   { 1, 1, 1, 1, 1, 1, 0, 0 }, // 0
@@ -28,15 +30,32 @@ void setup()
     /* code */
     pinMode(i,OUTPUT);
   }
-  
+  // Ponemos el pin del pulsador en modo INPUT (entradda)
+    pinMode(PULSASOR, INPUT);
+ 
+  //Establecemos la semilla fija
+  randomSeed(analogRead(A0));
 }
-
-
-
-
 
 void loop()
 {
   // put your main code here, to run repeatedly:
+  // Leemos el valor del pulsador
+  int valor =digitalRead(PULSASOR);
+
+  if (valor== HIGH)
+  {
+    /* code */
+    int ramdomNumber = random(1,7);
+
+    for ( e = 0; e <8; e++)
+    {
+      /* code */
+      digitalWrite(e+3,numero[ramdomNumber][e]);
+    }
+    _delay_ms(TIEMPO);
+  }
+  
+
 
 }
